3619bc8a0b29a6bd7b6a4a402c955e9a6577fa19
leegoway <leego.sir@gmail.com>
2016-10-24 16:58:51 +0800

    格式化输出

--- src/RestSerializer.php
+++ src/RestSerializer.php
@@ -1,18 +1,79 @@
 <?php
-
 namespace leegoway\rest;
 
 use yii\rest\Serializer;
 use Yii;
-use Yii\web\Response;
+use yii\base\Arrayable;
+use yii\base\Component;
+use yii\base\Model;
+use yii\data\DataProviderInterface;
+use yii\data\Pagination;
+use yii\helpers\ArrayHelper;
+use yii\web\Link;
+use yii\web\Request;
+use yii\web\Response;
 
 class RestSerializer extends Serializer
 {
 
+	public function init()
+	{
+		parent::init();
+		Yii::$app->response->format = Response::FORMAT_JSON;
+	}
+
     public function serialize($data)
     {
-        Yii::$app->response->format = Response::FORMAT_JSON;
-        $result = parent::serialize($data);
-        return ['code' => 200, 'msg' => '', 'data' => $result];  
+    	if ($data instanceof Model && $data->hasErrors()) {
+            return $this->serializeModelErrors($data);
+        } elseif ($data instanceof Arrayable) {
+            return $this->serializeModel($data);
+        } elseif ($data instanceof DataProviderInterface) {
+            return $this->serializeDataProvider($data);
+        } else {
+            return ['code' => 200, 'msg' => '', 'data' => $data, 'errors' => null];
+        }
     }
+
+    /**
+     * Serializes the validation errors in a model.
+     * @param Model $model
+     * @return array the array representation of the errors
+     */
+    protected function serializeModelErrors($model)
+    {
+        $errors = [];
+        foreach ($model->getFirstErrors() as $name => $message) {
+            $errors[] = [
+                'field' => $name,
+                'message' => $message,
+            ];
+        }
+        return ['code' => 400, 'msg' => null, 'data' => null, 'errors' => $errors]; 
+    }
+
+	/**
+     * Serializes a data provider.
+     * @param DataProviderInterface $dataProvider
+     * @return array the array representation of the data provider.
+     */
+    protected function serializeDataProvider($dataProvider)
+    {
+        $result = parent::serializeDataProvider($dataProvider);
+        return ['code' => 200, 'msg' => '', 'data' => $result, 'errors' => null]; 
+    }
+
+    /**
+     * Serializes a model object.
+     * @param Arrayable $model
+     * @return array the array representation of the model
+     */
+    protected function serializeModel($model)
+    {
+        $model = parent::serializeModel($model);
+        return ['code' => 200, 'msg' => '', 'data' => $model, 'errors' => null]; 
+    }
+
+
 }
+
