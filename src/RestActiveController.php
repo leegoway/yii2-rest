cb46633b1f7a732066daf87c97175668e37425c4
leegoway <leego.sir@gmail.com>
2016-11-08 16:14:00 +0800

    增加通过配置action的方式控制权限

--- src/RestActiveController.php
+++ src/RestActiveController.php
@@ -6,12 +6,13 @@
 namespace leegoway\rest;
 
 use yii\rest\ActiveController;
+use Yii;
 
 class RestActiveController extends ActiveController
 {
 
     public $serializer = 'leegoway\rest\RestSerializer';
-
+    public $uicRules = [];
 
     public function actions()
     {
@@ -22,6 +23,20 @@ class RestActiveController extends ActiveController
                 'checkAccess' => [$this, 'checkAccess'],
             ];
         return $actions;
+    }
+
+    public function checkAccess($action, $model = null, $params = [])
+    {
+        $allow = true;
+        if(isset($this->uicRules[$action])){
+            $organizationId = $this->uicRules[$action]['organizationId'];
+            $permissionId = $this->uicRules[$action]['permissionId'];
+            $allow = Yii::$app->uicAuther->checkPermission($permissionId, $organizationId);
+        }
+        if(!$allow){
+            throw new RestException('您没有权限执行此操作', 403);
+        }
+        return $allow;
     }
 }
 
